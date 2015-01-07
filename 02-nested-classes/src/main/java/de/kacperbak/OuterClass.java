859c283917f4ecc4a9f0787557504f61388018ee
Kacper Bak <github@kacperbak.de>
2015-01-07 19:45:19 +0100

    Inner- and static nested classes content added

---
+++ 02-nested-classes/src/main/java/de/kacperbak/OuterClass.java
@@ -0,0 +1,94 @@
+package de.kacperbak;
+
+/**
+ * Created by bakka on 07.01.15.
+ */
+public class OuterClass {
+
+    private static String outerFieldStatic = "myOuterFieldStatic";
+
+    private String outerField;
+
+    public OuterClass(String outerField) {
+        this.outerField = outerField;
+    }
+
+    public String getOuterField() {
+        return outerField;
+    }
+
+    public String useInnerInstance(){
+        InnerClass ic = new InnerClass("useInnerInstance");
+        return ic.getInnerField();
+    }
+
+    /**
+     * The name: StaticNestedInstance looks weird, but there are NO real static classes in Java!
+     * @return
+     */
+    public String useStaticNestedInstance(){
+        StaticNestedClass sc = new StaticNestedClass("useStaticNestedInstance");
+        return sc.getStaticNestedField();
+    }
+
+    public static String getOuterFieldStatic() {
+        return outerFieldStatic;
+    }
+
+    /**
+     * Inner class
+     * Can access OuterClass members
+     */
+    public class InnerClass{
+
+        //A constant (final) can be defined as static
+        public static final String CONST = "abc";
+
+        //A member can't be defined as static
+        private String innerField;
+
+        public InnerClass(String innerField) {
+            this.innerField = innerField;
+        }
+
+        public String getInnerField() {
+            return innerField;
+        }
+
+        public String fetchOuterField(){
+            return outerField;
+        }
+    }
+
+    /**
+     * Static nested class
+     * CAN'T access OuterClass members
+     */
+    public static class StaticNestedClass{
+
+        private String staticNestedField;
+
+        private static String staticNestedFieldStatic;
+
+        public StaticNestedClass(String staticNestedField) {
+            this.staticNestedField = staticNestedField;
+        }
+
+        public String getStaticNestedField() {
+            return staticNestedField;
+        }
+
+        public static void setStaticNestedFieldStatic(String staticNestedFieldStatic) {
+            StaticNestedClass.staticNestedFieldStatic = staticNestedFieldStatic;
+        }
+
+        public static String getStaticNestedFieldStatic() {
+            return staticNestedFieldStatic;
+        }
+
+        public static String getOuterField() {
+            return outerFieldStatic;
+        }
+    }
+
+}
--- 02-nested-classes/src/test/java/de/kacperbak/NestedClassesPlaygroundTest.java
+++ 02-nested-classes/src/test/java/de/kacperbak/NestedClassesPlaygroundTest.java
@@ -1,14 +1,44 @@
 package de.kacperbak;
 
+import junit.framework.Assert;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class NestedClassesPlaygroundTest {
 
+    private OuterClass oc = new OuterClass("myOuterField");
+
+    private OuterClass.InnerClass ic = oc.new InnerClass("myInnerField");
+
+    private OuterClass.StaticNestedClass sc = new OuterClass.StaticNestedClass("myStaticNestedField");
+
     @Test
-    public void testName() throws Exception {
+    public void useTheNestedClass() throws Exception {
+        //InnerClass
+        Assert.assertEquals("useInnerInstance", oc.useInnerInstance());
 
-
+        //StaticNestedClass
+        Assert.assertEquals("useStaticNestedInstance", oc.useStaticNestedInstance());
     }
+
+    @Test
+    public void accessPrivateMember() throws Exception {
+        //InnerClass
+        Assert.assertEquals(oc.getOuterField(), ic.fetchOuterField());
+
+        //StaticNestedClass
+        Assert.assertEquals(OuterClass.getOuterFieldStatic(), OuterClass.StaticNestedClass.getOuterField());
+    }
+
+    @Test
+    public void onlyConstantsAreAllowedAsStaticMembersInInnerClasses() throws Exception {
+        Assert.assertEquals("abc", OuterClass.InnerClass.CONST);
+    }
+
+    @Test
+    public void useStaticMember() throws Exception {
+        String expectedString = "myStaticNestedFieldStatic";
+        OuterClass.StaticNestedClass.setStaticNestedFieldStatic(expectedString);
+        Assert.assertEquals(expectedString, OuterClass.StaticNestedClass.getStaticNestedFieldStatic());
+    }
+
 }
