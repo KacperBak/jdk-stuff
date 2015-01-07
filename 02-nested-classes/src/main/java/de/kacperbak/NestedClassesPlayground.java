4533be4643a013211a48b2ca8a05df808238cc26
Kacper Bak <github@kacperbak.de>
2015-01-07 10:26:25 +0100

    Nested classes module created

---
+++ 02-nested-classes/src/main/java/de/kacperbak/NestedClassesPlayground.java
@@ -0,0 +1,8 @@
+package de.kacperbak;
+
+/**
+ * Created by bakka on 07.01.15.
+ */
+public class NestedClassesPlayground {
+
+}
---
+++ 02-nested-classes/src/test/java/de/kacperbak/NestedClassesPlaygroundTest.java
@@ -0,0 +1,14 @@
+package de.kacperbak;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NestedClassesPlaygroundTest {
+
+    @Test
+    public void testName() throws Exception {
+
+
+    }
+}
